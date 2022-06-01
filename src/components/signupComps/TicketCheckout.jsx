03f142162cc81d789299bc9c30b92c83a3adcfc2
Morten Six <mort108v@stud.kea.dk>
2022-06-01 15:31:15 +0200

    Small stuff

---
+++ src/components/signupComps/TicketCheckout.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function TicketCheckout({ formData, setFormData }) {
+  return (
+    <div className="ticket-checkout-container">
+      <div className="ticket-checkout-header">
+        <h2>Checkout</h2>
+      </div>
+      ;<div className="ticket-checkout-body"></div>;
+    </div>
+  );
+}
+
+export default TicketCheckout;
+
+// userEmail: "",
+// userPassword: "",
+// confirmPassword: false,
+// firstName: "",
+// lastName: "",
+// userName: "",
+// checkoutTotal: "",
+// authKey: "",
+// ticketType: "",
+// ticketDuration: "",
+// ticketDay: "",
+// agreeTerms: "",
+// agreeSocial: "",
+// ticketAmount: 0,
+// cardFee: 99,
+// campsite: "",
+// tentSetup: 0,
+// luxePack: 0,
+// greenCamp: 0,
+// faellesCard: 0,
+// extraGuests: [],
