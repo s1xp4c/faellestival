88ae3acf94f688fe610ae59aacd4e8f4e629373f
Morten Six <mort108v@stud.kea.dk>
2022-06-02 01:17:02 +0200

    New restDB and cleaned up push all data

--- src/components/signupComps/PushAllData.jsx
+++ src/components/signupComps/PushAllData.jsx
@@ -8,17 +8,31 @@ function PushAllData({ formData, setFormData }) {
     id: formData.authKey,
   };
 
+  const dataForRestDB = {
+    id: formData.authKey,
+    firstName: formData.firstName,
+    lastName: formData.lastName,
+    userEmail: formData.userEmail,
+    userName: formData.userName,
+    ticketType: formData.ticketType,
+    ticketDuration: formData.ticketDuration,
+    ticketDay: formData.ticketDay,
+    ticketAmount: formData.ticketAmount,
+    campsite: formData.campsite,
+    extraGuests: formData.extraGuests,
+  };
+
   const postFullfillData = JSON.stringify(fullfillID);
   console.log(postFullfillData);
 
-  const FAELLESTIVAL_FULLFILL_URL = import.meta.env
-    .VITE_FAELLESTIVAL_FULLFILL_RESERVATION;
-  const FAELLESTIVAL_RESTDB_URL = import.meta.env.VITE_FAELLESTIVAL_RESTDB_URL;
-  const FAELLESTIVAL_RESTDB_KEY = import.meta.env.VITE_FAELLESTIVAL_RESTDB_KEY;
+  const FAELLESTIVAL_FULFILL_URL = import.meta.env
+    .VITE_FAELLESTIVAL_FULFILL_RESERVATION;
+  const FAELLESTIVAL_RESTDB_URL = import.meta.env.VITE_FAEL_SEC_RESTDB_URL;
+  const FAELLESTIVAL_RESTDB_KEY = import.meta.env.VITE_FAEL_SEC_RESTDB_KEY;
 
   ////////////////////////////////////////////////////////////////////
 
-  console.log("Pushing ticket ID", FAELLESTIVAL_FULLFILL_URL);
+  console.log("Pushing ticket ID", FAELLESTIVAL_FULFILL_URL);
 
   useEffect(() => {
     // PUT request using fetch inside useEffect React hook
@@ -27,7 +41,7 @@ function PushAllData({ formData, setFormData }) {
       headers: { "Content-Type": "application/json; charset=utf-8" },
       body: postFullfillData,
     };
-    fetch(FAELLESTIVAL_FULLFILL_URL, pushIdRequestOptions)
+    fetch(FAELLESTIVAL_FULFILL_URL, pushIdRequestOptions)
       .then((response) => response.json())
       .then(
         (data) => setServerRespons(data),
@@ -46,8 +60,8 @@ function PushAllData({ formData, setFormData }) {
 
   //////////////////////////////////////////////////////////////////////
 
-  //   const postALLData = JSON.stringify(formData);
-  const postALLData = { ...formData };
+  const postALLData = JSON.stringify(dataForRestDB);
+  // const postALLData = { ...formData };
   console.log(postALLData);
 
   console.log("Pushing ALL data to RestDB", FAELLESTIVAL_RESTDB_URL);
@@ -71,7 +85,58 @@ function PushAllData({ formData, setFormData }) {
       );
   }, []);
 
-  return <div>CONGRATULATIONS!!</div>;
+  return (
+    <div className="congratulations-container">
+      <div className="congratulations-header">
+        <h3>CONGRATULATIONS!!</h3>
+      </div>
+      <div className="congratulations-body">
+        <div className="congratulations-info">
+          <span>*Here you see the info you submitted</span>
+        </div>
+        <div className="userName">
+          <span>Username: </span>
+        </div>
+        <div className="userName-db">{populateRestDB.userName}</div>
+        <div className="firstName">
+          <span>Firstname: </span>
+        </div>
+        <div className="firstName-db">{populateRestDB.firstName}</div>
+        <div className="lastName">
+          <span>Lastname: </span>
+        </div>
+        <div className="lastName-db">{populateRestDB.lastName}</div>
+        <div className="userEmail">
+          <span>Email: </span>
+        </div>
+        <div className="userEmail-db">{populateRestDB.userEmail}</div>
+        <div className="id">
+          <span>Password</span>
+        </div>
+        <div className="id-db">{populateRestDB.id}</div>
+        <div className="ticketType">
+          <span>Ticket Type: </span>
+        </div>
+        <div className="ticketType-db">{populateRestDB.ticketType}</div>
+        <div className="ticketDuration">
+          <span>Duration: </span>
+        </div>
+        <div className="ticketDuration-db">{populateRestDB.ticketDuration}</div>
+        <div className="ticketDay">
+          <span>Attendance Day: </span>
+        </div>
+        <div className="ticketDay-db">{populateRestDB.ticketDay}</div>
+        <div className="ticketAmount">
+          <span>People Going: </span>
+        </div>
+        <div className="ticketAmount-db">{populateRestDB.ticketAmount}</div>
+        <div className="campsite">
+          <span>Camping Area: </span>
+        </div>
+        <div className="campsite-db">{populateRestDB.campsite}</div>
+      </div>
+    </div>
+  );
 }
 
 export default PushAllData;
